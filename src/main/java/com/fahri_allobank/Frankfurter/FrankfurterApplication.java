ae623b03e37dab6d00fed2d029783203c7057849
fahrimuda12 <profesorrob01@gmail.com>
2026-01-09 19:46:36 +0700

    Initial Project

---
+++ src/main/java/com/fahri_allobank/Frankfurter/FrankfurterApplication.java
@@ -0,0 +1,13 @@
+package com.fahri_allobank.Frankfurter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankfurterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrankfurterApplication.class, args);
+	}
+
+}
